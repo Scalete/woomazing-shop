8ae89e63d9c047d24632d2027695d7727a87cf44
Maxim <maximpalaguta@gmail.com>
2023-01-18 00:39:17 +0200

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Promo } from './components/Promo';
+import { Header } from './components/Header';
 
 const App: React.FC = () => {
   return (
     <div className="App">
+        <Header/>
         <Promo/>
     </div>
   );
