a757068ae2155d6fb3be4c72054dc8bf3582deb2
Maxim <maximpalaguta@gmail.com>
2023-03-16 03:26:11 +0200

    fix urls

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,10 +5,10 @@ export const Menu: React.FC = () => {
     return (
         <nav>
             <ul className="menu">
-                <li><NavLink className='hover-underline-animation' to="/woomazing-shop">Главная</NavLink></li>
-                <li><NavLink className='hover-underline-animation' to="shop">Магазин</NavLink></li>
-                <li><NavLink className='hover-underline-animation' to="about">О бренде</NavLink></li>
-                <li><NavLink className='hover-underline-animation' to="contacts">Контакты</NavLink></li>
+                <li><NavLink className='hover-underline-animation' to="/">Главная</NavLink></li>
+                <li><NavLink className='hover-underline-animation' to="/shop">Магазин</NavLink></li>
+                <li><NavLink className='hover-underline-animation' to="/about">О бренде</NavLink></li>
+                <li><NavLink className='hover-underline-animation' to="/contacts">Контакты</NavLink></li>
             </ul>
         </nav>
     );
